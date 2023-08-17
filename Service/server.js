9cc3599809e8ff2270a4d2eb84757108208ea0d7
Manas Kumar Jena <manaskumar097@gmail.com>
2023-08-17 15:16:59 +0530

    updated from production

--- Service/server.js
+++ Service/server.js
@@ -49,14 +49,11 @@ app.use("/api", route);
 
 
 
-
-if (
-  process.env.NODE_ENV === "production") {
   app.use(express.static("ui"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(rootDir, "ui/index.html"));
   });
-}
 
 app.use((req, res, next) => {
   const error = new Error("Path not found.");
