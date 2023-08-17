324adfdda948a5deac45f0dc0f028fa27dc9a9aa
Manas Kumar Jena <manaskumar097@gmail.com>
2023-08-17 15:07:05 +0530

    server

--- Service/server.js
+++ Service/server.js
@@ -51,9 +51,7 @@ app.use("/api", route);
 
 
 if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "development"
-) {
+  process.env.NODE_ENV === "production") {
   app.use(express.static("ui"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(rootDir, "ui/index.html"));
