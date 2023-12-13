da7d772f3f1182213c48edae96ef49729bc0f382
Manas Kumar Jena <pixeldesignindia2023@gmail.com>
2023-12-13 11:59:03 +0530

    remove bankAccount number to string

--- Service/src/models/bankDetailsModel.js
+++ Service/src/models/bankDetailsModel.js
@@ -10,7 +10,7 @@ const accountSchema = new mongoose.Schema({
         required: true
     },
     bankAccountNo: {
-        type: Number,
+        type: String,
         required: true
     },
     city: {
